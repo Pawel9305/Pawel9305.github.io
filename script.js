0e0341ead5238ec6239796e9231b612d437375c7
pawelmalyska <pawel.malyska93@gmail.com>
2023-04-06 17:06:04 +0200

    adressess fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const apiRoot = 'https://Tasks.pawel9305.repl.co';
+  const trelloApiRoot = 'https://Tasks.pawel9305.repl.co';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
